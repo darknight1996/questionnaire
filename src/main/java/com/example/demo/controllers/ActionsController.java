306ebf7c11660b1df3467cb52222a9637b657c66
darkn <darknight1996.nk@gmail.com>
2024-10-01 23:00:32 +0200

    brief controllers refactor

--- src/main/java/com/example/demo/controllers/ActionsController.java
+++ src/main/java/com/example/demo/controllers/ActionsController.java
@@ -8,7 +8,6 @@ import com.example.demo.services.ResponseService;
 import com.example.demo.services.UserService;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -20,32 +19,38 @@ import java.util.Map;
 @Controller
 public class ActionsController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @Autowired
-    private FieldService fieldService;
+    private final FieldService fieldService;
 
-    @Autowired
-    private ResponseService responseService;
+    private final ResponseService responseService;
+
+    public ActionsController(final UserService userService, final FieldService fieldService, final ResponseService responseService) {
+        this.userService = userService;
+        this.fieldService = fieldService;
+        this.responseService = responseService;
+    }
 
     @PostMapping("/registration")
-    public String addUser(User user) {
+    public String addUser(final User user) {
         if (!userService.addUser(user)) {
             return "redirect:/registration?error";
         }
+
         return "redirect:/login";
     }
 
     @PostMapping("/profile")
-    public String editUser(User user, String oldUsername) {
+    public String editUser(final User user, final String oldUsername) {
         userService.editUser(user, oldUsername);
+
         return "redirect:/";
     }
 
     @PostMapping("/password")
-    public String changePassword(String currentPassword, String newPassword1, String newPassword2) {
-        String message = userService.changePassword(currentPassword, newPassword1, newPassword2);
+    public String changePassword(final String currentPassword, final String newPassword1, final String newPassword2) {
+        final String message = userService.changePassword(currentPassword, newPassword1, newPassword2);
+
         if (message.isEmpty()) {
             return "redirect:/";
         } else {
@@ -54,31 +59,36 @@ public class ActionsController {
     }
 
     @PostMapping("/fields/add")
-    public String addField(Field field, String options) {
+    public String addField(final Field field, final String options) {
         field.setOptions(new ArrayList<>(Arrays.asList(options.split("\n"))));
+
         fieldService.addField(field);
+
         return "redirect:/fields";
     }
 
     @PostMapping("/fields/edit")
-    public String editField(Long id, String label, FieldType type, String options, boolean required, boolean active) {
+    public String editField(final Long id, final String label, final FieldType type, final String options, final boolean required, final boolean active) {
         fieldService.editField(id, label, type, new ArrayList<>(Arrays.asList(options.split("\n"))), required, active);
+
         return "redirect:/fields";
     }
 
     @PostMapping("/fields/delete")
-    public String deleteField(Long id) {
+    public String deleteField(final Long id) {
         fieldService.deleteField(id);
+
         return "redirect:/fields";
     }
 
     @PostMapping(value = "/response")
-    public String addResponse(String map) {
-        System.out.println(map);
-        Type type = new TypeToken<Map<Long, String>>(){}.getType();
-        Gson gson = new Gson();
-        Map<Long, String> myMap = gson.fromJson(map, type);
+    public String addResponse(final String map) {
+        final Type type = new TypeToken<Map<Long, String>>(){}.getType();
+        final Gson gson = new Gson();
+        final Map<Long, String> myMap = gson.fromJson(map, type);
+
         responseService.addResponse(myMap);
+
         return "redirect:/";
     }
 
--- src/main/java/com/example/demo/controllers/PagesController.java
+++ src/main/java/com/example/demo/controllers/PagesController.java
@@ -2,99 +2,109 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Field;
 import com.example.demo.models.Response;
+import com.example.demo.models.User;
 import com.example.demo.services.FieldService;
 import com.example.demo.services.ResponseService;
 import com.example.demo.services.UserService;
-import com.example.demo.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
 @Controller
 public class PagesController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @Autowired
-    private FieldService fieldService;
+    private final FieldService fieldService;
 
-    @Autowired
-    private ResponseService responseService;
+    private final ResponseService responseService;
+
+    public PagesController(final UserService userService, final FieldService fieldService, final ResponseService responseService) {
+        this.userService = userService;
+        this.fieldService = fieldService;
+        this.responseService = responseService;
+    }
 
     @GetMapping("/registration")
     public String registration() {
         return "registration";
     }
 
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
     @GetMapping("/profile")
-    public String profile(Model model) {
-        User user = userService.getCurrentUser();
+    public String profile(final Model model) {
+        final User user = userService.getCurrentUser();
+
         model.addAttribute("user", user);
+
         return "profile";
     }
 
     @GetMapping("/password")
-    public String password(Model model) {
-        User user = userService.getCurrentUser();
+    public String password(final Model model) {
+        final User user = userService.getCurrentUser();
+
         model.addAttribute("user", user);
+
         return "password";
     }
 
     @GetMapping("/fields")
-    public String fields(Model model) {
+    public String fields(final Model model) {
+        final User user = userService.getCurrentUser();
 
-        User user = userService.getCurrentUser();
         model.addAttribute("user", user);
 
-        List<Field> fields = fieldService.getAllFields();
+        final List<Field> fields = fieldService.getAllFields();
+
         model.addAttribute("fields", fields);
 
         return "fields";
     }
 
     @GetMapping("/responses")
-    public String responses(Model model) {
-        User user = userService.getCurrentUser();
-        List<Response> responses = responseService.getResponses();
-        List<List<String>> responsesForRender = new ArrayList<>();
-        List<Field> activeFields = fieldService.getActiveFields();
+    public String responses(final Model model) {
+        final List<Response> responses = responseService.getResponses();
+        final List<Field> activeFields = fieldService.getActiveFields();
+
+        final List<List<String>> responsesForRender = new ArrayList<>();
+
         responses.forEach(response -> {
-            List<String> list = new ArrayList<>();
+            final List<String> answersForRender = new ArrayList<>();
             activeFields.forEach(field -> {
-                String answer = response.getMap().get(field.getId());
-                if (answer != null) {
-                    list.add(answer);
-                } else {
-                    list.add("N/A");
-                }
+                final String answer = response.getMap().get(field.getId());
+                answersForRender.add(Objects.requireNonNullElse(answer, "N/A"));
             });
-            responsesForRender.add(list);
+            responsesForRender.add(answersForRender);
 
         });
-        responsesForRender.forEach(System.out::println);
+
+        final User user = userService.getCurrentUser();
+
         model.addAttribute("user", user);
         model.addAttribute("fields", activeFields);
         model.addAttribute("responses", responsesForRender);
+
         return "responses";
     }
 
     @GetMapping("/")
-    public String response(Model model) {
+    public String response(final Model model) {
+        final User user = userService.getCurrentUser();
+        final List<Field> activeFields = fieldService.getActiveFields();
 
-        List<Field> fields = fieldService.getActiveFields();
-        model.addAttribute("fields", fields);
-
-        User user = userService.getCurrentUser();
         model.addAttribute("user", user);
+        model.addAttribute("fields", activeFields);
 
         return "response";
     }
+
 }
